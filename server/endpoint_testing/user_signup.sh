da202dc222128a6f9c887376be2c24a362948fe9
ashmit <ashmitbhardwaj05@gmail.com>
2024-05-21 19:27:00 +0530

    revised folder structure and get notes functionality

---
+++ server/endpoint_testing/user_signup.sh
@@ -0,0 +1,2 @@
+curl -X POST -d '{"name":"Ashmit","email":"ashmitbhardwaj05@gmail.com","password":"abcd3fasd"}' http://localhost:8080/signup
+
---
+++ server/routes/router.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/Ashmit-05/notefy/controllers"
+)
+
+func SetUserRoutes(router *http.ServeMux) {
+	router.HandleFunc("POST /signup",controllers.SignUp)
+	router.HandleFunc("POST /signin",controllers.SignIn)
+}
+
+func SetNoteRoutes(router *http.ServeMux) {
+	router.HandleFunc("POST /note",controllers.CreateNote)
+	router.HandleFunc("GET /notes",controllers.GetNotes)
+}
